0c8a9982d4ef78fef98f78aab65e7584a2cc2d42
tejaat1995 <ktejaat1995@gmail.com>
2019-09-18 01:48:38 +0530

    sidemenu resolved

--- src/app/CustomerDashboard/customer-login/customer-login.page.ts
+++ src/app/CustomerDashboard/customer-login/customer-login.page.ts
@@ -1,56 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { DatabaseService } from '../../services/database/database.service';
-import { CommonService } from '../../services/common/common.service';
-import { Router } from '@angular/router';
-
-
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { DatabaseService } from "../../services/database/database.service";
+import { CommonService } from "../../services/common/common.service";
+import { Router } from "@angular/router";
+import { MenuController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-customer-login',
-  templateUrl: './customer-login.page.html',
-  styleUrls: ['./customer-login.page.scss'],
+  selector: "app-customer-login",
+  templateUrl: "./customer-login.page.html",
+  styleUrls: ["./customer-login.page.scss"]
 })
 export class CustomerLoginPage implements OnInit {
-
   public customercredentails: FormGroup;
-  
-  constructor(private database: DatabaseService, 
-			  private formBuilder: FormBuilder, 
-			  private router: Router, 
-			  private commonService: CommonService) { 
-			  			  
-	  this.customercredentails = this.formBuilder.group({
-      username: ['', Validators.compose([Validators.required])],
-      password: ['', Validators.compose([Validators.required])]
-		})
-	}
 
-  ngOnInit() {
-  }
-	
-	
-  submit(formValue) {
-    this.database.customerIsValidUser(formValue).subscribe((customerloginResp: any) => {
-      console.log('my cust response:::::::\n', customerloginResp );
-      if (!customerloginResp.Success) {
-        this.commonService.presentToast(customerloginResp.ErrorMessage);
-      } else {
-        this.commonService.presentToast(customerloginResp.ErrorMessage);
-        console.log('my resp:::::::\n', customerloginResp + formValue)
-        this.router.navigate(['/customer-dashboard'])
-      }
-    }, err => {
-      this.commonService.presentToast(" Error while calling API");
+  constructor(
+    private database: DatabaseService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private commonService: CommonService,
+    private menuCtrl: MenuController
+  ) {
+    this.customercredentails = this.formBuilder.group({
+      username: ["", Validators.compose([Validators.required])],
+      password: ["", Validators.compose([Validators.required])]
     });
   }
-  
+
+  ngOnInit() {}
+
+  submit(formValue) {
+    this.database.customerIsValidUser(formValue).subscribe(
+      (customerloginResp: any) => {
+        console.log("my cust response:::::::\n", customerloginResp);
+        if (!customerloginResp.Success) {
+          this.commonService.presentToast(customerloginResp.ErrorMessage);
+        } else {
+          this.commonService.presentToast(customerloginResp.ErrorMessage);
+          console.log("my resp:::::::\n", customerloginResp + formValue);
+          this.menuCtrl.enable(true);
+          this.router.navigate(["/customer-dashboard"]);
+        }
+      },
+      err => {
+        this.commonService.presentToast(" Error while calling API");
+      }
+    );
+  }
+
   navToForgetPassword() {
-    this.router.navigate(['/customer-forgetpass'])
+    this.router.navigate(["/customer-forgetpass"]);
   }
 
   createCustomerRegDetails() {
-    this.router.navigate(['/customer-reg'])
+    this.router.navigate(["/customer-reg"]);
   }
-  
 }
